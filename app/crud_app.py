1da49526791abeef716d22a4b3ddecf6589e4de3
Lily <lily@>
2017-07-21 18:07:37 -0400

    Print a menu which contains a greeting message and a hard-coded number of products and a list of available operations.

--- app/crud_app.py
+++ app/crud_app.py
@@ -1,1 +1,15 @@
-
+menu = """
+-----------------------------------
+PRODUCTS APPLICATION
+-----------------------------------
+Welcome @lilyyyyyyyyy!
+There are 20 products in the database. Please select an operation:
+    operation | description
+    --------- |------------------
+    'List'    | Display a list of product identifiers and names.
+    'Show'    | Show information about a product.
+    'Create'  | Add a new product.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
+ """
+print(menu)
