c5a3c217a4dd7fbc9ab579c305be7be3bd52ac43
Lily <lily@>
2017-07-21 18:18:55 -0400

    Demonstrate your ability to print the entire contents of the inventory CSV file.

--- app/crud_app.py
+++ app/crud_app.py
@@ -1,41 +1,55 @@
-menu = """
------------------------------------
-PRODUCTS APPLICATION
------------------------------------
-Welcome @lilyyyyyyyyy!
-There are 20 products in the database. Please select an operation:
-    operation | description
-    --------- |------------------
-    'List'    | Display a list of product identifiers and names.
-    'Show'    | Show information about a product.
-    'Create'  | Add a new product.
-    'Update'  | Edit an existing product.
-    'Destroy' | Delete an existing product.
- """
+import csv
 
-chosen_operation = input(menu)
-chosen_operation = chosen_operation.title()
+products = []
 
-def list_product():
-    print("List")
-def show_product():
-    print("Show")
-def create_product():
-    print("Create")
-def update_product():
-    print("Update")
-def destroy_product():
-    print("Destroy")
+csv_file_path = "/Users/lily/Desktop/CRUD-Application/data/products.csv"
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        products.append(row)
+
+print(products)
 
 
-if chosen_operation == "List":
-    list_product()
-elif chosen_operation == "Show":
-    show_product()
-elif chosen_operation == "Create":
-    create_product()
-elif chosen_operation == "Update":
-    update_product()
-elif chosen_operation == "Destroy":
-    destroy_product()
-else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+# menu = """
+# -----------------------------------
+# PRODUCTS APPLICATION
+# -----------------------------------
+# Welcome @lilyyyyyyyyy!
+# There are 20 products in the database. Please select an operation:
+#     operation | description
+#     --------- |------------------
+#     'List'    | Display a list of product identifiers and names.
+#     'Show'    | Show information about a product.
+#     'Create'  | Add a new product.
+#     'Update'  | Edit an existing product.
+#     'Destroy' | Delete an existing product.
+#  """
+#
+# chosen_operation = input(menu)
+# chosen_operation = chosen_operation.title()
+#
+# def list_product():
+#     print("List")
+# def show_product():
+#     print("Show")
+# def create_product():
+#     print("Create")
+# def update_product():
+#     print("Update")
+# def destroy_product():
+#     print("Destroy")
+#
+#
+# if chosen_operation == "List":
+#     list_product()
+# elif chosen_operation == "Show":
+#     show_product()
+# elif chosen_operation == "Create":
+#     create_product()
+# elif chosen_operation == "Update":
+#     update_product()
+# elif chosen_operation == "Destroy":
+#     destroy_product()
+# else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
